70bf7be357854092863d24caec946c3bb9ee1795
yige666 <yige29805@gmail.com>
2024-04-26 16:58:15 +0800

    37对象字符串

--- Source/ActionRouglelike/ActionRouglelikeGameModeBase.cpp
+++ Source/ActionRouglelike/ActionRouglelikeGameModeBase.cpp
@@ -17,5 +17,11 @@ void AActionRouglelikeGameModeBase::SelfDestoryActor()
 	//if(MyActor) MyActor->SetLifeSpan(1.5);
 	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("TEST"),true,FVector2D(5,5));
 	//UE_LOG(LogTemp, Log, TEXT("ok"));
+
+	const char* test = "helloworld";
+	TCHAR* tmp = ANSI_TO_TCHAR(test);	// TCHAR是UE中的传统字符串
+	UE_LOG(LogTemp, Log, TEXT("%s"), tmp);
+
 	UE_LOG(LogTemp, Log, TEXT("i am %d years old, height is %f cm, come from %s"), FMath::RandRange(0, 100), FMath::FRandRange(100, 200), TEXT("北京"));
-} 
+}  
+ 
